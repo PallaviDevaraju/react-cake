f0455e7a7a390170f5e72f49f0c698ad854c25c0
PallaviDevaraju <pallavidevaraju07@gmail.com>
2023-11-17 10:47:27 +0530

    first commit

---
+++ src/component/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Header.css"
+
+function Header() {
+  return (
+    <div>
+      <h1 id='head'>Cake Shop</h1>
+      
+     
+    </div>
+  )
+}
+
+export default Header
---
+++ src/component/Header1.js
@@ -0,0 +1,15 @@
+import React from 'react';
+class Header1 extends React.Component
+{
+    render()
+    {
+        return(
+            <div>
+                <h1>GIFT SHOP</h1>
+            </div>
+        )
+    }
+}
+
+
+export default Header1
